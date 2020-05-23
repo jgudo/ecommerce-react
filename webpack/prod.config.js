bb06c33a9f2a56592b61dec64fae0fdf8a94661e
Julius Guevarra <juliusguevarra101@gmail.com>
2020-05-23 10:19:38 -0700

    fix 404 page

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -55,6 +55,18 @@ module.exports = merge(baseConfig, {
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
     }),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, '../dist/404.html'),
+      template: '404.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    }),
     new CopyWebpackPlugin([
       {
         from: path.resolve(__dirname, '../static'),
@@ -82,7 +94,7 @@ module.exports = merge(baseConfig, {
     // })
 
     new workboxPlugin.GenerateSW({
-      cacheId: 'salinaka-ecommerce_v2', // change this
+      cacheId: 'salinaka-ecommerce_v3', // change this
       swDest: 'sw.js',
       navigateFallback: '/index.html',
       navigateFallbackWhitelist: [ /^\/[^\_]+\/?/ ],
