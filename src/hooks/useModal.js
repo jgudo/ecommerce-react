d8f925f4b37356601349656e1b6802720ed3ae56
Julius Guevarra <juliusguevarra101@gmail.com>
2020-07-26 16:45:32 +0800

    Use hook for modals

---
+++ src/hooks/useModal.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const useModal = () => {
+	const [isOpenModal, setModalOpen] = useState(false);
+
+	const onOpenModal = () => {
+		setModalOpen(true);
+	};
+
+	const onCloseModal = () => {
+		setModalOpen(false);
+	};
+
+	return { isOpenModal, onOpenModal, onCloseModal };
+};
+
+export default useModal;
