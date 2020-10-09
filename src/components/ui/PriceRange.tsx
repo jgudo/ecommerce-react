51564406a187afd0b7e507adecda16b499927a8e
Julius Guevarra <juliusguevarra101@gmail.com>
2020-10-09 17:23:14 +0800

    more changes

--- src/components/ui/PriceRange.tsx
+++ src/components/ui/PriceRange.tsx
@@ -1,48 +1,52 @@
-import React, { useState, useEffect, useRef, ChangeEvent } from 'react';
-import { displayMoney } from 'helpers/utils';
+import React, { useState } from 'react'
+import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider';
+import { SliderRail, Handle, Track, Tick } from './PriceRangeComponents';
+
+const sliderStyle: React.CSSProperties = {
+	position: 'relative',
+	width: '100%',
+}
 
 interface IProps {
-	initMin: number;
-	initMax: number;
 	min: number;
 	max: number;
+	initMin: number;
+	initMax: number;
+	onPriceChange: (...val: number[]) => void;
 	productsLength: number;
-	onPriceChange: (min: number, max: number) => void;
 }
 
 const PriceRange: React.FC<IProps> = (props) => {
-	const [minState, setMinState] = useState(props.initMin ? props.initMin : props.min);
-	const [maxState, setMaxState] = useState(props.initMax ? props.initMax : props.max);
-	const [inputError, setInputError] = useState(false);
-	const slider = useRef<HTMLDivElement>(null);
-	const rangeMin = useRef<HTMLInputElement>(null);
-	const rangeMax = useRef<HTMLInputElement>(null);
+	const [state, setState] = useState({
+		domain: [props.min, props.max],
+		values: [props?.initMin || props.min, props?.initMax || props.max],
+		update: [props.min, props.max].slice(),
+		inputMin: props?.initMin || props.min,
+		inputMax: props?.initMax || props.max,
+		inputError: false,
+		reversed: false,
+	})
 
-	useEffect(() => {
-		setMinState(props.initMin ? props.initMin : props.min);
-		setMaxState(props.initMax ? props.initMax : props.max);
-	}, [props.initMin, props.initMax]);
+	const onUpdate = (update) => {
+		setState(() => ({ ...state, update, inputMin: update[0], inputMax: update[1] }));
+	}
 
-	const onRangeChange = () => {
-		let slide1 = rangeMin.current ? +rangeMin.current.value : 0;
-		let slide2 = rangeMax.current ? +rangeMax.current.value : 0;
+	const onChange = (values) => {
+		setState(() => ({ ...state, values, inputMin: values[0], inputMax: values[1] }));
+		if (values[0] < values[1]) props.onPriceChange(...values);
+	}
 
-		if (slide1 > slide2) {
-			[slide1, slide2] = [slide2, slide1];
-		}
+	const setDomain = domain => {
+		setState({ ...state, domain })
+	}
 
-		setMinState(slide1);
-		setMaxState(slide2);
-		setInputError(false);
-		props.onPriceChange(slide1, slide2);
+	const inputClassName = () => {
+		return state.inputError ? 'price-range-input price-input-error' : 'price-range-input';
 	};
 
-	const onInputMinChange = (e: ChangeEvent<HTMLInputElement>) => setMinState(Number(e.target.value));
-	const onInputMaxChange = (e: ChangeEvent<HTMLInputElement>) => setMaxState(Number(e.target.value));
-
 	const onBlurInput = () => {
-		let valMin = +minState;
-		let valMax = +maxState;
+		let valMin = +state.inputMin;
+		let valMax = +state.inputMax;
 
 		if (valMin < props.min) {
 			valMin = props.min;
@@ -51,25 +55,27 @@ const PriceRange: React.FC<IProps> = (props) => {
 		}
 
 		if (valMin > valMax) {
-			setInputError(true);
+			setState({ ...state, inputError: true });
 		} else {
-			setInputError(false);
+			setState({ ...state, inputError: false });
 		}
 
-		setMinState(valMin);
-		setMaxState(valMax);
-		props.onPriceChange(valMin, valMax);
+		setState({ ...state, inputMin: valMin, inputMax: valMax, values: [valMin, valMax] });
+		if (valMin < valMax) props.onPriceChange(valMin, valMax);
 	};
 
-	const inputClassName = () => {
-		return inputError ? 'price-range-input price-input-error' : 'price-range-input';
-	};
+	const onInputMinChange = (e) => {
+		const val = e.target.value;
+		setState({ ...state, inputMin: val, values: [val, state.values[1]] });
+	}
+
+	const onInputMaxChange = (e) => {
+		const val = e.target.value;
+		setState({ ...state, inputMax: val, values: [state.values[0], 1] });
+	}
 
 	return (
-		<div
-			className="price-range"
-			ref={slider}
-		>
+		<div style={{ height: 120, width: '100%' }}>
 			<div className="price-range-control">
 				<input
 					className={inputClassName()}
@@ -79,10 +85,10 @@ const PriceRange: React.FC<IProps> = (props) => {
 					onBlur={onBlurInput}
 					onChange={onInputMinChange}
 					type="number"
-					value={minState}
+					value={state.inputMin}
 				/>
-				—
-				<input
+	—
+			<input
 					className={inputClassName()}
 					disabled={props.productsLength === 0}
 					max={props.max}
@@ -90,39 +96,62 @@ const PriceRange: React.FC<IProps> = (props) => {
 					onBlur={onBlurInput}
 					onChange={onInputMaxChange}
 					type="number"
-					value={maxState}
+					value={state.inputMax}
 				/>
 			</div>
-			<div className="price-range-control">
-				<input
-					className="price-range-slider"
-					disabled={props.productsLength === 0}
-					max={props.max}
-					min={props.min}
-					onChange={onRangeChange}
-					ref={rangeMin}
-					step="50"
-					type="range"
-					value={minState}
-				/>
-				<input
-					className="price-range-slider"
-					disabled={props.productsLength === 0}
-					max={props.max}
-					min={props.min}
-					onChange={onRangeChange}
-					ref={rangeMax}
-					step="20"
-					type="range"
-					value={maxState}
-				/>
-			</div>
-			<div className="price-range-scale">
-				<span className="price-range-price">MIN: {displayMoney(props.min)}</span>
-				<span className="price-range-price">MAX: {displayMoney(props.max)}</span>
-			</div>
+			<Slider
+				mode={1}
+				step={1}
+				domain={state.domain}
+				rootStyle={sliderStyle}
+				onUpdate={onUpdate}
+				onChange={onChange}
+				values={state.values}
+			>
+				<Rail>
+					{({ getRailProps }) => <SliderRail getRailProps={getRailProps} />}
+				</Rail>
+				<Handles>
+					{({ handles, activeHandleID, getHandleProps }) => (
+						<div className="slider-handles">
+							{handles.map((handle) => (
+								<Handle
+									key={handle.id}
+									handle={handle}
+									domain={state.domain}
+									isActive={handle.id === activeHandleID}
+									getHandleProps={getHandleProps}
+								/>
+							))}
+						</div>
+					)}
+				</Handles>
+				<Tracks left={false} right={false}>
+					{({ tracks, getTrackProps }) => (
+						<div className="slider-tracks">
+							{tracks.map(({ id, source, target }) => (
+								<Track
+									key={id}
+									source={source}
+									target={target}
+									getTrackProps={getTrackProps}
+								/>
+							))}
+						</div>
+					)}
+				</Tracks>
+				<Ticks count={5}>
+					{({ ticks }) => (
+						<div className="slider-ticks">
+							{ticks.map(tick => (
+								<Tick key={tick.id} tick={tick} count={ticks.length} />
+							))}
+						</div>
+					)}
+				</Ticks>
+			</Slider>
 		</div>
 	);
-};
+}
 
 export default PriceRange;
---
+++ src/components/ui/PriceRangeComponents.tsx
@@ -0,0 +1,316 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { GetHandleProps, HandlesProps, SliderItem } from 'react-compound-slider';
+
+// *******************************************************
+// TOOLTIP RAIL
+// *******************************************************
+const railStyle: React.CSSProperties = {
+	position: 'absolute',
+	width: '100%',
+	transform: 'translate(0%, -50%)',
+	height: 20,
+	cursor: 'pointer',
+	zIndex: 300
+	// border: '1px solid grey',
+};
+
+const railCenterStyle: React.CSSProperties = {
+	position: 'absolute',
+	width: '100%',
+	transform: 'translate(0%, -50%)',
+	height: 14,
+	borderRadius: 7,
+	cursor: 'pointer',
+	pointerEvents: 'none',
+	backgroundColor: '#d0d0d0'
+};
+
+interface IProps {
+	activeHandleID: string;
+	getEventData: any;
+	getRailProps: (props: any) => void;
+}
+
+export class TooltipRail extends Component<IProps> {
+	static defaultProps = {
+		disabled: false
+	};
+
+	state = {
+		value: null,
+		percent: null
+	};
+
+	onMouseEnter = () => {
+		document.addEventListener('mousemove', this.onMouseMove);
+	}
+
+	onMouseLeave = () => {
+		this.setState({ value: null, percent: null })
+		document.removeEventListener('mousemove', this.onMouseMove);
+	}
+
+	onMouseMove = (e: MouseEvent) => {
+		const { activeHandleID, getEventData } = this.props;
+
+		if (activeHandleID) {
+			this.setState({ value: null, percent: null });
+		} else {
+			this.setState(getEventData(e));
+		}
+	}
+
+	render() {
+		const { value, percent } = this.state;
+		const { activeHandleID, getRailProps } = this.props;
+
+		return (
+			<Fragment>
+				{!activeHandleID && value ? (
+					<div
+						style={{
+							left: `${percent}%`,
+							position: 'absolute',
+							marginLeft: '-11px',
+							marginTop: '-35px',
+						}}
+					>
+						<div className="tooltip">
+							<span className="tooltiptext">Value: {value}</span>
+						</div>
+					</div>
+				) : null}
+				<div
+					style={railStyle}
+					{...getRailProps({
+						onMouseEnter: this.onMouseEnter,
+						onMouseLeave: this.onMouseLeave,
+					})}
+				/>
+				<div style={railCenterStyle} />
+			</Fragment>
+		);
+	}
+}
+
+// *******************************************************
+// SLIDER RAIL (no tooltips)
+// *******************************************************
+const railOuterStyle: React.CSSProperties = {
+	position: 'absolute',
+	transform: 'translate(0%, -50%)',
+	width: '100%',
+	height: 42,
+	borderRadius: 7,
+	cursor: 'pointer',
+	// border: '1px solid grey',
+};
+
+const railInnerStyle: React.CSSProperties = {
+	position: 'absolute',
+	width: '100%',
+	height: 14,
+	transform: 'translate(0%, -50%)',
+	borderRadius: 7,
+	pointerEvents: 'none',
+	backgroundColor: '#d0d0d0',
+};
+
+export function SliderRail({ getRailProps }) {
+	return (
+		<Fragment>
+			<div style={railOuterStyle} {...getRailProps()} />
+			<div style={railInnerStyle} />
+		</Fragment>
+	);
+}
+
+SliderRail.propTypes = {
+	getRailProps: PropTypes.func.isRequired,
+};
+
+// *******************************************************
+// HANDLE COMPONENT
+// *******************************************************
+
+interface IHandleProps {
+	domain: number[];
+	handle: SliderItem;
+	isActive: boolean;
+	disabled: boolean;
+	getHandleProps: any;
+}
+
+export class Handle extends Component<IHandleProps> {
+	static defaultProps = {
+		disabled: false
+	};
+
+	state = {
+		mouseOver: false
+	};
+
+	onMouseEnter = () => {
+		this.setState({ mouseOver: true });
+	}
+
+	onMouseLeave = () => {
+		this.setState({ mouseOver: false });
+	}
+
+	render() {
+		const {
+			domain: [min, max],
+			handle: { id, value, percent },
+			isActive,
+			disabled,
+			getHandleProps
+		} = this.props;
+		const { mouseOver } = this.state;
+
+		return (
+			<Fragment>
+				{(mouseOver || isActive) && !disabled ? (
+					<div
+						style={{
+							left: `${percent}%`,
+							position: 'absolute',
+							marginLeft: '-11px',
+							marginTop: '-35px',
+						}}
+					>
+						<div className="tooltip">
+							<span className="tooltiptext">Value: {value}</span>
+						</div>
+					</div>
+				) : null}
+				<div
+					style={{
+						left: `${percent}%`,
+						position: 'absolute',
+						transform: 'translate(-50%, -50%)',
+						WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+						zIndex: 400,
+						width: 26,
+						height: 42,
+						cursor: 'pointer',
+						// border: '1px solid grey',
+						backgroundColor: 'none',
+					}}
+					{...getHandleProps(id, {
+						onMouseEnter: this.onMouseEnter,
+						onMouseLeave: this.onMouseLeave,
+					})}
+				/>
+				<div
+					role="slider"
+					aria-valuemin={min}
+					aria-valuemax={max}
+					aria-valuenow={value}
+					style={{
+						left: `${percent}%`,
+						position: 'absolute',
+						transform: 'translate(-50%, -50%)',
+						WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+						zIndex: 300,
+						width: 24,
+						height: 24,
+						border: 0,
+						borderRadius: '50%',
+						boxShadow: '1px 1px 1px 1px rgba(0, 0, 0, 0.2)',
+						backgroundColor: disabled ? '#666' : '#1a1a1a',
+					}}
+				/>
+			</Fragment>
+		);
+	}
+}
+
+// *******************************************************
+// TRACK COMPONENT
+// *******************************************************
+export function Track({ source, target, getTrackProps, disabled }) {
+	return (
+		<div
+			style={{
+				position: 'absolute',
+				transform: 'translate(0%, -50%)',
+				height: 14,
+				zIndex: 1,
+				backgroundColor: disabled ? '#ffd993' : '#ffa500',
+				borderRadius: 7,
+				cursor: 'pointer',
+				left: `${source.percent}%`,
+				width: `${target.percent - source.percent}%`,
+			}}
+			{...getTrackProps()}
+		/>
+	);
+}
+
+Track.propTypes = {
+	source: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		value: PropTypes.number.isRequired,
+		percent: PropTypes.number.isRequired,
+	}).isRequired,
+	target: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		value: PropTypes.number.isRequired,
+		percent: PropTypes.number.isRequired,
+	}).isRequired,
+	getTrackProps: PropTypes.func.isRequired,
+	disabled: PropTypes.bool,
+};
+
+Track.defaultProps = {
+	disabled: false,
+};
+
+// *******************************************************
+// TICK COMPONENT
+// *******************************************************
+export function Tick({ tick, count, format }) {
+	return (
+		<div>
+			<div
+				style={{
+					position: 'absolute',
+					marginTop: 17,
+					width: 1,
+					height: 5,
+					backgroundColor: 'rgb(200,200,200)',
+					left: `${tick.percent}%`,
+				}}
+			/>
+			<div
+				style={{
+					position: 'absolute',
+					marginTop: 25,
+					fontSize: 10,
+					textAlign: 'center',
+					marginLeft: `${-(100 / count) / 2}%`,
+					width: `${100 / count}%`,
+					left: `${tick.percent}%`,
+				}}
+			>
+				{format(tick.value)}
+			</div>
+		</div>
+	);
+}
+
+Tick.propTypes = {
+	tick: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		value: PropTypes.number.isRequired,
+		percent: PropTypes.number.isRequired,
+	}).isRequired,
+	count: PropTypes.number.isRequired,
+	format: PropTypes.func.isRequired
+};
+
+Tick.defaultProps = {
+	format: d => d
+};
