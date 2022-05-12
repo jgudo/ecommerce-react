941978fcce03a47e9ef088bea1b3b879e3133a07
will363 <williamtokam78@gmail.com>
2022-05-12 23:22:05 +0100

    #[1] Initialisation de mon projet Lunetas-cam

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
 
   return !visibleOnlyPath.includes(pathname) ? null : (
     <footer className="footer">
-      <div className="footer-col-1">
+      {/* <div className="footer-col-1">
         <strong>
           <span>
             Developed by
@@ -21,7 +21,7 @@ const Footer = () => {
             <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
           </span>
         </strong>
-      </div>
+      </div> */}
       <div className="footer-col-2">
         <img alt="Footer logo" className="footer-logo" src={logo} />
         <h5>
@@ -29,14 +29,14 @@ const Footer = () => {
           {new Date().getFullYear()}
         </h5>
       </div>
-      <div className="footer-col-3">
+      {/* <div className="footer-col-3">
         <strong>
           <span>
             Fork this project &nbsp;
             <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
           </span>
         </strong>
-      </div>
+      </div> */}
     </footer>
   );
 };
